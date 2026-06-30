86c5ea89da93401e74b31c205ced3af89099be81
Gael <a01713218@tec.mx>
2026-06-29 19:45:26 -0600

    fix: desactiva minificacion HTML para codigo legible

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: false,
+  compress: false,
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
